9067939c69098e89f4b94947bd25da608d6afe28
AlhonGelios <Geliosal39hon@gmail.com>
2020-09-24 13:53:53 +0300

    add_card

--- src/js/modules/checkTextInputs.js
+++ src/js/modules/checkTextInputs.js
@@ -8,19 +8,19 @@ const checkTextInputs = (selector) => {
             }
         });
         
-        // input.addEventListener('change' , () => {
-        //     if (input.value.split('').filter(item => item.match(/[^а-яё 0-9]/ig)).length) {
-        //         input.value = '';
-        //     }        
-        // });
-
         input.addEventListener('change' , () => {
-            try {
-                if (input.value.match(/[^а-яё 0-9]/ig).length) {
-                    input.value = '';
-                } 
-            } catch (e) {console.log(e);}       
+            if (input.value.split('').filter(item => item.match(/[^а-яё 0-9]/ig)).length) {
+                input.value = '';
+            }        
         });
+
+        // input.addEventListener('change' , () => {
+        //     try {
+        //         if (input.value.match(/[^а-яё 0-9]/ig).length) {
+        //             input.value = '';
+        //         } 
+        //     } catch (e) {console.log(e);}       
+        // });
     });
 };
 
